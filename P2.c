c9241c19f4b9ea57566e5ff1f735f4075063cc8c
Luiz <luiz@lexCorp>
2018-04-25 13:08:44 -0300

    Agora ele observa o tamanho do T_echo

--- P2.c
+++ P2.c
@@ -7,9 +7,10 @@
  * main.c
  */
 
-#define TA1_CCRN_INT 52
-#define VELOCIDADE_SOM 32500 // m/s
+#define TA0_CCR0_INT 53
+#define VELOCIDADE_SOM 32500 // cm/s
 #define FREQUENCIA 1000000 // Hz
+#define TRIGGER_TEMPO 10 // us
 
 void debounce(){
 	volatile int i=1000000;
@@ -18,44 +19,55 @@ void debounce(){
 
 uint16_t distancia = 0; // Em centímetros
 
-#pragma vector=TA1_CCRN_INT
-__interrupt void TA1_CCRN_ISR(){
-	switch(TA1IV){
-		case 0x04: // CCR2
-		/*  Distância é VELOCIDADE_SOM*(tempo até o eco)/2 */
-		distancia = (VELOCIDADE_SOM*TA0CCR2/FREQUENCIA) << 1;
+/* Interrupção executada quando echo sobe/desce */
+#pragma vector=TA0_CCR0_INT
+__interrupt void TA0_CCR0_ISR(){
+	if(TA0CCTL & SCCI){ // Valor da última captura
+		/* !TODO Como calcula distância com TA0CCR0? */
 	}
+	else {
+		distancia = 0;
+		TA0CCTL |= (TACLR | MC__CONT); // Começa a contar no TA0
+	}
+	TA0CCTL0 &= ~CCIFG;
+}
+
+void geraSinalTrigger(){
+	
+	/* Faz trigger ser 1 por 10us */
+	TA1CCTL1 = OUTMOD_5; // Reset
+	TA1CCR1 = TRIGGER_TEMPO-1; // Ideal, conta de 0 a 9 (10us)
+	
+	/* Timer para trigger, saída */
+	TA1CCTL = (TACLR | TASSEL__SMCLK | MC__CONT);
+	
 }
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
     
 	/* Configura o botão S1 (P2.1) */
-	P2REN |= BIT1; // Resistor de 
-	P2OUT |= BIT1; // Pull-up
+	P2REN |= BIT1;  // Resistor de 
+	P2OUT |= BIT1;  // Pull-up
 	P2DIR &= ~BIT1; // Entrada
 	
-	/* Se for SMCLK, conta a cada 1us = 0,001ms: de 0ms a ~60ms, para
-	   20cm e 40cm e usando 325m/s a velocidade do som deve-se contar até 
-	   0,60ms e 1,20ms, dá para usar SMCLK */
+	/* Timer para echo, entrada
+	   Se for SMCLK, conta a cada 1us = 0,001ms: de 0ms a ~60ms 
+	 */
 	TA0CCTL = (TACLR | TASSEL__SMCLK | MC__HOLD);
+	
+	__enable_interrupt();
 	
 	while(1){
 		// Amostragem para S1
 		if(!(P2IN & BIT1)){
-		
-			TA0CCTL |= TACLR;
-			distancia = 0;
 			
-			/* Faz trigger ser 1 por 10us, podemos usar o timer A */
-			TA0CCTL1 = OUTMOD_5; // Reset
-			TA0CCR1 = 9; // Ideal, conta de 0 a 9 (10us)
+			geraSinalTrigger();
 			
-			/* Deve-se ligar a porta P1.2 no trigger */
-			TA0CCTL |= MC__CONT;
+			TA1CCTL |= TACLR;
 			
-			/* Deve-se ligar o sinal de echo na porta P1.3 */
-			TA0CCTL2 = (CAP | CM_1 | CCIS_A | CCIE);
+			/* Deve-se ligar o sinal de echo na porta P1.1 */
+			TA0CCTL0 = (CAP | CM_3 | CCIS_A | CCIE);
 					
 			debounce();
 			while(!(P2IN & BIT1)); // Aguarda soltar
