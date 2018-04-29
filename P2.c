0661ca79a1d3a86508b24a1f6b5af29ff9d213f3
Luiz <luiz@lexCorp>
2018-04-29 19:42:27 -0300

    Agora os LEDs são alterados conforme especificação

--- P2.c
+++ P2.c
@@ -24,8 +24,24 @@ uint16_t inicio = 0; // Em us
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void TA0_CCR0_ISR(){
     if(TA0CTL & SCCI){ // Valor da última captura
-        distancia = (TA0CCR0-inicio)/58; // 58 está em uma documentação
-        //printf("Resultado: %ucm\n", distancia);
+    
+        /* 58 está em uma documentação */
+        distancia = (TA0CCR0-inicio)/58;
+        
+        /* Muda estado dos LEDs */
+        if(distancia < 20) {
+            P1OUT |= BIT0;
+            P4OUT &= ~BIT7;
+        }
+        else if(distancia >= 20 && distancia <= 40) {
+            P1OUT &= ~BIT0;
+            P4OUT |= BIT7;
+        }
+        else {
+            P1OUT |= BIT0;
+            P4OUT |= BIT7;
+        }
+        
     }
     else {
         inicio = TA0CCR0;
@@ -41,13 +57,18 @@ void main(void) {
     P2REN |= BIT1;  // Resistor de 
     P2OUT |= BIT1;  // Pull-up
     P2DIR &= ~BIT1; // Entrada
+    
+    /* Configura o LED vermelho */
+    P1OUT &= ~BIT0; // Desligado
+    P1DIR |= BIT0;  // Saída
+    
+    /* Configura o LED verde */
+    P4OUT &= ~BIT7; // Desligado
+    P4DIR |= BIT7;  // Saída
                 
     /* Deve-se ligar o sinal de echo na porta P1.1 */
     TA0CCTL0 = (CAP | CM_3 | CCIS_0 | CCIE);
     
-    /* Deve-se ligar o sinal de trigger na porta P1.2 */
-    TA0CCTL1 = OUTMOD_5; // Reset
-    TA0CCR1 = TRIGGER_TEMPO-1; // Conta 10us
     
     __enable_interrupt();
     
@@ -55,14 +76,18 @@ void main(void) {
         // Amostragem para S1
         if(!(P2IN & BIT1)){
             
-            TA0CTL |= (TACLR | TASSEL__SMCLK | MC__CONTINUOUS); // Zera e configura timer A
-                    
+            /* Zera e configura timer A */
+            TA0CTL |= (TACLR | TASSEL__SMCLK | MC__CONTINUOUS);
+            
+            /* Deve-se ligar o sinal de trigger na porta P1.2 */
+            TA0CCTL1 = OUTMOD_5; // Reset
+            TA0CCR1 = TRIGGER_TEMPO-1; // Conta 10us
+            
             debounce();
             while(!(P2IN & BIT1)); // Aguarda soltar
             debounce();
             
         }
-    
     }
     
 }
