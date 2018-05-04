7966f2098835afd344b1e461839c0c568167cae1
Luiz <luiz@lexCorp>
2018-05-03 21:17:20 -0300

    Agora tem um makefile e a configuração dos clocks

---
+++ makefile
@@ -0,0 +1,28 @@
+IDIR=include
+SDIR=src
+ODIR=obj
+CC=msp430-gcc
+CFLAGS=-I $(IDIR) -mmcu=msp430f5529
+LIBS=
+
+# Dependências
+
+_DEPS=clock.h pmm.h
+DEPS=$(patsubst %,$(IDIR)/%,$(_DEPS))
+
+# Módulos
+
+_OBJ=clock.o pmm.o main.o
+OBJ=$(patsubst %,$(ODIR)/%,$(_OBJ))
+
+install: $(OBJ) $(TEST)
+	$(CC) -o main.bin $^ $(CFLAGS) $(LIBS)
+
+$(ODIR)/%.o: $(SDIR)/%.c $(DEPS)
+	$(CC) -c -o $@ $< $(CFLAGS)
+
+build:
+	if [ ! -d $ODIR ]; then mkdir $(ODIR); fi
+
+clean:
+	rm -f $(ODIR)/*.o main.bin
---
+++ src/main.c
@@ -0,0 +1,104 @@
+#include <msp430.h>
+#include <stdint.h>
+#include "clock.h"
+#include "pmm.h"
+
+#define S1_DELAY 0xffff
+#define TRIGGER_TIME 10
+
+uint16_t valorInicial;
+
+void delay(uint16_t time){
+    /* TA0 */
+    TA0CTL = (TACLR | MC__CONTINUOUS | TASSEL__SMCLK);
+    while(TA0R != time);
+}
+
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void TA1_CCR0_ISR(){
+    if(TA1CCTL0 & CCI) valorInicial = TA1CCR0; // Echo subiu
+    else {
+        uint16_t diferenca = TA1CCR0-valorInicial;
+        
+        if(diferenca < 1160) {
+            P1OUT |= BIT0;
+            P4OUT &= ~BIT7;
+        }
+        else if(diferenca >= 1160 && diferenca <= 2320) {
+            P1OUT &= ~BIT0;
+            P4OUT |= BIT7;
+        }
+        else {
+            P1OUT |= BIT0;
+            P4OUT |= BIT7;
+        }
+    }
+    TA1CCTL0 &= ~TAIFG;
+}
+
+
+void main()
+{
+    // Stop watchdog timer
+	WDTCTL = WDTPW | WDTHOLD;
+
+	// Increase core voltage so CPU can run at 16Mhz
+	// Step 1 allows clocks up to 12MHz, step 2 allows rising MCLK to 16MHz
+	pmmVCore(1);                             
+	pmmVCore(2);
+
+	// Configure clock
+    // This should make MCLK @16MHz, SMCLK @1MHz and ACLK @32768Hz
+	clockInit();
+
+	// Your program here...
+	
+	
+    /* S1 */
+    P2REN |=  BIT0;
+    P2OUT |=  BIT0; // Pull-up
+    P2DIR &= ~BIT0; // Entrada
+    
+    /* LED1 */
+    P1OUT &= ~BIT0;
+    P1DIR |=  BIT0; // Saída
+    
+    /* LED2 */
+    P4OUT &= ~BIT7;
+    P4DIR |=  BIT7; // Saída
+    
+    /* P1.7 (TA1.0)*/
+    P1SEL |=  BIT7; // Funcionalidade dedicada
+    P1DIR &= ~BIT7; // Entrada
+    
+    /* P2.0 (TA1.1) */
+    P2SEL |=  BIT0; // Funcionalidade dedicada
+    P2DIR |=  BIT0; // Saída
+    
+    __enable_interrupt();
+    
+    while(1){
+        if(!(P2IN & BIT0)){
+        
+            /* TA1 */
+            TA1CTL = (TACLR | MC__STOP | TASSEL__SMCLK);
+            
+            /* TA1.0 (echo) */
+            TA1CCTL0 = (CAP | CM_3 | CCIS_0 | CCIE);
+            
+            /* Inicia contagem */
+            TA1CTL |= MC__CONTINUOUS;
+            
+            /* TA1.1 (trigger) */
+            TA1CCTL1 = OUTMOD_5; // Modo reset
+            TA1CCR1 = TRIGGER_TIME-1;
+            
+            /* Debounce */
+            delay(S1_DELAY);
+            while(!(P2IN & BIT0));
+            delay(S1_DELAY);
+        }
+    }
+
+
+}
